1ebaeb145ca403981cdc99830346dae9067d2415
Nancy Ahmed <nancyahmed2025@gmail.com>
2021-02-15 16:15:28 +0200

    compelete login component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import Header from './components/Header/Header';
 import Footer from "./components/Footer/Footer";
 // import Contact from "./components/Contact/Contact";
-import SignUp from "./components/SignUp/SignUp";
-
+// import SignUp from "./components/SignUp/SignUp";
+import Login from "./components/Login/Login";
 function App() {
   return (
     <>
       <Header/>
-      <SignUp/>
+      <Login/>
 
       <Footer/>
     </>
