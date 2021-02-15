8a186a7b151dad671d754c7a1a7ed4680b777fef
Nancy Ahmed <nancyahmed2025@gmail.com>
2021-02-15 15:42:11 +0200

    complete signup component and edit margin

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from "./components/Footer/Footer";
-import Contact from "./components/Contact/Contact";
-
+// import Contact from "./components/Contact/Contact";
+import SignUp from "./components/SignUp/SignUp";
 
 function App() {
   return (
     <>
       <Header/>
-      <Contact/>
+      <SignUp/>
 
       <Footer/>
     </>
