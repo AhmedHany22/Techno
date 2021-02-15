98582747f6109a47243693983a196774e4aaab04
AmdHany22 <ahmed_hany19@outlook.com>
2021-02-15 13:56:20 +0200

    Complete Footer Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
+      <Footer></Footer>
     </>
   );
 }
