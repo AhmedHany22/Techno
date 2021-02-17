d6f2d47c3a2106eb6350044d48dd9879c6b930cf
eman20m <eman20mohamed10@gmail.com>
2021-02-17 13:23:43 +0200

    routes and validation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import './App.css';
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './Routes';
 import Header from './components/Header/Header';
-import Footer from "./components/Footer/Footer";
-import Checkout from "./components/Checkout/Checkout";
-import Contact from "./components/Contact/Contact";
-import SignUp from "./components/SignUp/SignUp";
-import SignIn from "./components/SignIn/SignIn";
-import About from "./components/About/About";
-import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
+import Footer from './components/Footer/Footer';
+
+
 function App() {
   return (
-    <>
-      <Header/>
-      <About/>
-      <Footer/>
-    </>
+
+    <Router>
+      <Header />
+      <Routes />
+      <Footer />
+    </Router>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,58 +1,70 @@
 import "./Header.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Link } from 'react-router-dom';
 
 const Header = () => {
 
   return (
     <>
-    <nav class="navbar navbar-expand-sm navbar-light py-3 bg-white">
-       <div class="container">
-         <a class="navbar-brand font-weight-bold" href="../Nancy/home.html">
-           <h2>Techno</h2>
-         </a>
-         <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
-           <span class="navbar-toggler-icon"></span>
-         </button>
-         <div class="collapse navbar-collapse" id="collapsibleNavId">
-           <ul class="navbar-nav m-auto mt-2 mt-lg-0">
-             <li class="nav-item">
-               <a class="nav-link mx-4 text-dark myNav" href="../Nancy/home.html">Home</a>
-             </li>
-             <li class="nav-item">
-               <a class="nav-link mx-4 text-dark myNav" href="../Aisha/about.html">About</a>
-             </li>
-             <li class="nav-item">
-               <a class="nav-link mx-4 text-dark myNav" href="../Aisha/shop.html">Shop</a>
-             </li>
-             <li class="nav-item">
-               <a class="nav-link mx-4 text-dark myNav" href="../Eman/contant.html">Contact</a>
-             </li>
-           </ul>
-           <div class="d-flex float-right navbar-expand-sm ">
-             <ul class="navbar-nav float-right ">
-               <li class="nav-item dropdown">
-                 <a class="nav-link dropdown-toggle icon" href="#" id="navbardrop" data-toggle="dropdown">
-                   <i class="fa fa-user-circle"></i>
-                 </a>
-                 <div class="dropdown-menu">
-                   <a class="dropdown-item" href="../Eman/login.html">Sign in</a>
-                   <a class="dropdown-item" href="../Nancy/signup.html">Sign up</a>
-                 </div>
-               </li>
-               <li class="nav-item dropdown">
-                 <a class="nav-link dropdown-toggle icon" href="#" id="navbardrop" data-toggle="dropdown">
-                   <i class="fa fa-shopping-cart"></i>
-                 </a>
-                 <div class="dropdown-menu">
-                   <a class="dropdown-item" href="Shopping-Cart.html">View cart</a>
-                   <a class="dropdown-item" href="Checkout.html">Checkout</a>
-                 </div>
-               </li>
-             </ul>
-           </div>
-         </div>
-       </div>
-     </nav>
+      <nav class="navbar navbar-expand-sm navbar-light py-3 bg-white">
+        <div class="container">
+          <a class="navbar-brand font-weight-bold" href="../Nancy/home.html">
+            <h2>Techno</h2>
+          </a>
+          <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav m-auto mt-2 mt-lg-0">
+              <li class="nav-item">
+                {/*} <a class="nav-link mx-4 text-dark myNav" href="../Nancy/home.html">Home</a>*/}
+                <Link to="/">Home</Link>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link mx-4 text-dark myNav" href="/">About</a>
+              </li>
+              <li class="nav-item">
+                {/*  <a class="nav-link mx-4 text-dark myNav" href="#"><Link to="/Shop">Shop</Link></a>*/}
+                <Link className="nav-link" href="/">Shop</Link>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link mx-4 text-dark myNav" href="/Contact">Contact</a>
+              </li>
+            </ul>
+            <div class="d-flex float-right navbar-expand-sm ">
+              <ul class="navbar-nav float-right ">
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle icon" href="#" id="navbardrop" data-toggle="dropdown">
+                    <i class="fa fa-user-circle"></i>
+                  </a>
+                  <div class="dropdown-menu">
+                    {/*<a class="dropdown-item" href="../Eman/login.html">Sign in</a>*/}
+                    <Link className="nav-link" to="/signin">Sign in</Link>
+
+                    {/* <a class="dropdown-item" href="../Nancy/signup.html">Sign up</a>*/}
+                    <Link className="nav-link" to="/signup">Sign up</Link>
+
+                  </div>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle icon" href="#" id="navbardrop" data-toggle="dropdown">
+                    <i class="fa fa-shopping-cart"></i>
+                  </a>
+                  <div class="dropdown-menu">
+                    {/*<a class="dropdown-item" href="Shopping-Cart.html">View cart</a>*/}
+                    <Link className="nav-link" to="/shoppingCart">View cart</Link>
+
+                    {/*<a class="dropdown-item" href="Checkout.html">Checkout</a>*/}
+                    <Link className="nav-link" to="/checkout">Checkout</Link>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+
+      </nav>
     </>
   );
 };
